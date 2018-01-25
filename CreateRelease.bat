f9d54ab1b2f4439fd10e65d3bd319e01412ba9da
ANDREY SHATALOV <shatalov160188@gmail.com>
2018-01-25 13:10:03 +0300

    New dependencies for release

--- CreateRelease.bat
+++ CreateRelease.bat
@@ -7,16 +7,19 @@ xcopy /y Release\Exporter.dll NewRelease\x86\Binaries
 xcopy /y Release\CppCoverage.dll NewRelease\x86\Binaries
 xcopy /y Release\Tools.dll NewRelease\x86\Binaries
 xcopy /y Release\libctemplate.dll NewRelease\x86\Binaries
-xcopy /y Release\template_test_util_test.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_filesystem-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_program_options-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_system-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_log-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_thread-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_regex-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_date_time-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_locale-vc120-mt-1_55.dll NewRelease\x86\Binaries
-xcopy /y Release\boost_chrono-vc120-mt-1_55.dll NewRelease\x86\Binaries
+xcopy /y Release\libprotobuf.dll NewRelease\x86\Binaries
+xcopy /y Release\libprotobuf-lite.dll NewRelease\x86\Binaries
+xcopy /y Release\zlib1.dll NewRelease\x86\Binaries
+xcopy /y Release\libbz2.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_filesystem-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_program_options-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_system-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_log-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_thread-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_iostreams-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_date_time-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_locale-vc141-mt-1_64.dll NewRelease\x86\Binaries
+xcopy /y Release\boost_chrono-vc141-mt-1_64.dll NewRelease\x86\Binaries
 
 mkdir NewRelease\x86\Pdb
 xcopy /y Release\OpenCppCoverage.pdb NewRelease\x86\Pdb
@@ -32,16 +35,19 @@ xcopy /y x64\Release\Exporter.dll NewRelease\x64\Binaries
 xcopy /y x64\Release\CppCoverage.dll NewRelease\x64\Binaries
 xcopy /y x64\Release\Tools.dll NewRelease\x64\Binaries
 xcopy /y x64\Release\libctemplate.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\template_test_util_test.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_filesystem-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_program_options-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_system-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_log-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_thread-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_regex-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_date_time-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_locale-vc120-mt-1_55.dll NewRelease\x64\Binaries
-xcopy /y x64\Release\boost_chrono-vc120-mt-1_55.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\libprotobuf.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\libprotobuf-lite.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\zlib1.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\libbz2.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_filesystem-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_program_options-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_system-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_log-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_thread-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_iostreams-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_date_time-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_locale-vc141-mt-1_64.dll NewRelease\x64\Binaries
+xcopy /y x64\Release\boost_chrono-vc141-mt-1_64.dll NewRelease\x64\Binaries
 
 mkdir NewRelease\x64\Pdb
 xcopy /y x64\Release\OpenCppCoverage.pdb NewRelease\x64\Pdb
